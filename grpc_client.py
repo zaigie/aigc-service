1fb398b305c2cfe5eb34d9b23d0c2e96114eb17d
JoyceWen <jokerwho@yeah.net>
2023-02-09 10:29:07 +0800

    feat: update grpc_client more like a complete program

--- grpc_client.py
+++ grpc_client.py
@@ -3,27 +3,86 @@ import chat_pb2
 import chat_pb2_grpc
 import sys
 import uuid
+import readline
 
-prompt = "胰十二指肠手术的术后护理"
+
+def get_input(prompt):
+    print(prompt, end="")
+    lines = []
+    while True:
+        line = input()
+        if line == "":
+            break
+        lines.append(line)
+    user_input = "\n".join(lines)
+    return user_input
 
 
-def run():
-    with grpc.insecure_channel("localhost:9100") as channel:
+def run(url):
+    with grpc.insecure_channel(url) as channel:
         client = chat_pb2_grpc.ChatStub(channel)
-        sys.stdout.flush()
-        try:
-            for i in client.Ask(
-                chat_pb2.askrequest(
-                    prompt=prompt,
-                    conversation_id=f"grpc_{uuid.uuid4().hex[:8]}",
+        conversation_id = f"grpc_{uuid.uuid4().hex[:8]}"
+        print("连接成功！你可以向他询问任何问题啦！")
+        print(
+            "=======================================\nNote: 因为支持多行输入，若确定了问题，请按两次回车键哦。\n======================================="
+        )
+        while True:
+            try:
+                prompt = get_input("\n你：\n")
+            except UnicodeDecodeError:
+                print(
+                    "=======================================\n你刚刚的输入包含特殊字符，无法识别。\n======================================="
                 )
-            ):
-                print(i.response, end="")
+                continue
+            except KeyboardInterrupt:
+                print("\n退出中...")
+                sys.exit()
+            print("\nChatGPT: ")
+            sys.stdout.flush()
+            try:
                 sys.stdout.flush()
-        except Exception as e:
-            print("Error: %s" % e)
-            print("出了点问题，请查验并重试")
+                for i in client.Ask(
+                    chat_pb2.askrequest(
+                        prompt=prompt,
+                        conversation_id=conversation_id,
+                    )
+                ):
+                    print(i.response, end="")
+                    sys.stdout.flush()
+                print()
+            except Exception as e:
+                # print("Error: %s" % e)
+                # print("访问人数多，请重试")
+                try:
+                    sys.stdout.flush()
+                    for i in client.Ask(
+                        chat_pb2.askrequest(
+                            prompt=prompt,
+                            conversation_id=conversation_id,
+                        )
+                    ):
+                        print(i.response, end="")
+                        sys.stdout.flush()
+                    print()
+                except:
+                    print("访问人数过多，请重试")
+                    continue
 
 
 if __name__ == "__main__":
-    run()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--url",
+        type=str,
+        required=True,
+        help="gRPC URL e.g. localhost:9000",
+    )
+    args = parser.parse_args()
+    url = args.url
+    print(f"初始化: 正在连接到位于 {url} 的 ChatGPT...")
+    try:
+        run(url)
+    except (KeyboardInterrupt, EOFError):
+        sys.exit()
